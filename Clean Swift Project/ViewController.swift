b1fe0a31f879771eeb3a8faa9fdfe040cd7d2c86
Israel3D <israelmesquita07@gmail.com>
2019-05-14 09:44:21 -0300

    Initial Commit

---
+++ Clean Swift Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clean Swift Project
+//
+//  Created by Israel3D on 14/05/2019.
+//  Copyright © 2019 Israel3D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Clean Swift ProjectTests/Clean_Swift_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  Clean_Swift_ProjectTests.swift
+//  Clean Swift ProjectTests
+//
+//  Created by Israel3D on 14/05/2019.
+//  Copyright © 2019 Israel3D. All rights reserved.
+//
+
+import XCTest
+@testable import Clean_Swift_Project
+
+class Clean_Swift_ProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
